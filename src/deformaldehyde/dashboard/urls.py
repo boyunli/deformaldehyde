b18f37065a44b40580d4895d1ffad0a2f913b64f
liling <liling@meifang.com>
2018-09-07 12:41:46 +0800

    change gitignore

--- src/deformaldehyde/dashboard/urls.py
+++ src/deformaldehyde/dashboard/urls.py
@@ -7,6 +7,6 @@ app_name = "dashboard"
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
     path(r'tag/<slug:tag_name>.html', views.TagDetailView.as_view(), name='tag_detail'),
-    path(r'tag/<slug:tag_name>.html', views.AreaTagView.as_view(), name='area_tag'),
+    path(r'area/<slug:tag_name>.html', views.AreaTagView.as_view(), name='area_tag'),
     path(r'category/<slug:category_name>.html', views.ArchiveView.as_view(), name='article_category'),
 ]
